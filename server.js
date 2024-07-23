fbeb92520e9d544766669f73d6484a688b81e2b0
Sarthak S <sarthaks@Sarthaks-Laptop.local>
2024-07-23 21:55:37 +0530

    Added limit

--- server.js
+++ server.js
@@ -7,20 +7,35 @@ require('dotenv').config();
 const app = express();
 const port = 3000;
 
+// Store IP usage
+const ipUsage = new Map();
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json({limit: '50mb'}));
 
-app.post('/upload', async (req, res) => {
+// Middleware to check IP usage
+const checkIpUsage = (req, res, next) => {
+    const ip = req.ip;
+    const usageCount = ipUsage.get(ip) || 0;
+    
+    if (usageCount >= 5) {
+        return res.status(429).json({ error: 'Usage limit exceeded' });
+    }
+    
+    ipUsage.set(ip, usageCount + 1);
+    next();
+};
+
+app.post('/upload', checkIpUsage, async (req, res) => {
     const { image1, image2 } = req.body;
 
     try {
-        // Convert base64 to the required format
         const base64ToDataUrl = (base64) => {
             return `data:application/octet-stream;base64,${base64.split(',')[1]}`;
         };
 
-        const base_image = base64ToDataUrl(image1);  // reference image
-        const identity_image = base64ToDataUrl(image2);  // selfie image
+        const base_image = base64ToDataUrl(image1);
+        const identity_image = base64ToDataUrl(image2);
 
         const input = {
             base_image: base_image,
@@ -30,7 +45,7 @@ app.post('/upload', async (req, res) => {
             base_image_strength: parseFloat(req.body.base_image_strength)
         };
 
-        const replicateApiKey = process.env.REPLICATE_API_KEY; // Replace with your actual API key
+        const replicateApiKey = process.env.REPLICATE_API_KEY;
         const replicateApiUrl = 'https://api.replicate.com/v1/predictions';
 
         const response = await fetch(replicateApiUrl, {
@@ -52,11 +67,9 @@ app.post('/upload', async (req, res) => {
         const prediction = await response.json();
         console.log('Prediction:', prediction);
 
-        // Check if the prediction is completed
         if (prediction.status === 'succeeded') {
             res.json({ message: 'Images processed successfully', output: prediction.output });
         } else {
-            // If not completed, send the id for polling
             res.json({ message: 'Processing started', id: prediction.id });
         }
     } catch (error) {
@@ -67,7 +80,7 @@ app.post('/upload', async (req, res) => {
 
 app.get('/check-status/:id', async (req, res) => {
     try {
-      const replicateApiKey = process.env.REPLICATE_API_KEY;      // Replace with your actual API key
+        const replicateApiKey = process.env.REPLICATE_API_KEY;
         const replicateApiUrl = `https://api.replicate.com/v1/predictions/${req.params.id}`;
 
         const response = await fetch(replicateApiUrl, {
